8446777f5c525e3e43d87845fa5877ed9001ca16
Matt Dwen <matt@group6.co.nz>
2014-03-20 10:45:19 +1300

    Fix database deployment

--- app/templates/puppet/modules/silverstripe/manifests/init.pp
+++ app/templates/puppet/modules/silverstripe/manifests/init.pp
@@ -3,11 +3,11 @@
 class silverstripe::install {
 
 	# import database for basic silverstripe site
-	file { '/tmp/silverstripe-db.sql':
+	file { '/tmp/db.sql':
 		source => 'puppet:///modules/silverstripe/silverstripe-db.sql'
 	}
 
 	exec { 'load-db':
-		command => '/usr/bin/mysql -u vagrant -pvagrant vagrant < /tmp/silverstripe-db.sql'
+		command => '/usr/bin/mysql -u vagrant -pvagrant vagrant < /tmp/db.sql'
 	}
 }
